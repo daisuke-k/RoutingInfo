2c95bb1404f695f0a635207d4bb02a134d0f6e1c
Daisuke Kotani <kotani@media.kyoto-u.ac.jp>
2019-05-02 17:08:34 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="RoutingInfo",
+    version="0.0.1",
+    description="Accessing arbitrary routing information",
+    author="Daisuke Kotani",
+    install_requires=["mrtparse", "py-radix"],
+)
